61fbe5b1312e2b987891994ad989221db1c41cbe
selva-notionpress <selva@notionpress.com>
2015-05-21 10:50:50 +0530

    selva branch 1st commit

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<form>
-<input type="radio" name="sex" value="male" checked>Male
-<br>
-<input type="radio" name="sex" value="female">Female
-</form> 
-
-</body>
-</html>
+<?php 
+echo "selva branch";
+?>
